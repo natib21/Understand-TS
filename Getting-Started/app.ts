b33d0686281ed34e429dd2a6ee02ad72cd0a7099
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-09 00:54:55 -0800

    Data Type and type script

--- Getting-Started/app.ts
+++ Getting-Started/app.ts
@@ -1,1 +1,16 @@
-console.log("Your app Goes here.... ")
+function add(n1:number,n2:number,showResult:boolean,phrase :string){
+
+    const result = n1 + n2;
+    if(showResult){
+        console.log(phrase + result)
+    }else{
+        return n1 + n2;
+    }
+}
+const num1 = 20;
+const num2 = 35;
+const printResult = true
+const resultPhrase= 'Result is: '
+
+add(num1,num2,printResult,resultPhrase)
+
