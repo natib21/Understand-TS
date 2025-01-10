c82239bfa8c7bc4c17dee38a7b4501b16a508724
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-10 00:12:21 -0800

    Function Return Type

--- Getting-Started/app.ts
+++ Getting-Started/app.ts
@@ -1,3 +1,8 @@
+///////////////////////////////////////////////////
+
+// 1. This is a function that describe data type in function parameter 
+
+
 function add(n1:number,n2:number,showResult:boolean,phrase :string){
 
     const result = n1 + n2;
@@ -14,13 +19,42 @@ const resultPhrase= 'Result is: '
 
 add(num1,num2,printResult,resultPhrase)
 
+
+
 // Type Inference is a process that automatically assigns types to variables, functions, and constants in a program
+////////////////////////////////////////////////////////////
+
+// Type Aliases
+function Aliases(){
+
+    type Combinable = number | string
+    
+    function combine(input1 : Combinable, input2 : Combinable){
+        let result;
+        if (typeof input1 === 'number' && typeof input2 === 'number'){
+            result = input1 + input2
+        }else {
+            result = input1.toString() + input2.toString()
+        }
+        
+        return result
+    }
+    
+    
+    const combineInput = combine(10,20)
+    console.log(combineInput)
+    
+    const combineInputS = combine("Nati","Betty")
+    console.log(combineInputS)
+}
+
+
+
+/////////////////////////////////////////////////////////////
 
 // Union Types
 
-type Combinable = number | string
-
-function combine(input1 : Combinable, input2 : Combinable){
+function combine(input1 : number | string, input2 : number | string){
     let result;
     if (typeof input1 === 'number' && typeof input2 === 'number'){
         result = input1 + input2
@@ -37,3 +71,23 @@ console.log(combineInput)
 
 const combineInputS = combine("Nati","Betty")
 console.log(combineInputS)
+
+
+////////////////////////////////////////////////////////////
+
+// Function Return Type 
+
+function adder(n1:number,n2:number):number{
+    return n1 + n2
+}
+
+// void doesn't return anything 
+
+function printResult2(num:number):void{
+    console.log('result: '+num)
+}
+
+printResult2(adder(10,34))
+
+
+/////////////////////////////////////////////////////////////
