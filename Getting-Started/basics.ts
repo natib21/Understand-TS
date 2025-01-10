383ad3b39706b1be2c94d884f7043a2ed6a8a7db
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-09 22:37:51 -0800

    enum

--- Getting-Started/basics.ts
+++ Getting-Started/basics.ts
@@ -1,17 +1,17 @@
-const person:{
-    name: string;
-    age: number;
-    hobbies:string[];
-    role:any[]
-} = {
+
+enum Role {ADMIN,READ_ONLY,AUTHOR}
+const person = {
     name: "Nathnael",
     age:  28,
     hobbies:["Sport","Cooking"],
-    role:[2,'author']
+    role:Role.AUTHOR
 }
 
 console.log(person.name)
 
 for (const hobby of person.hobbies){
     console.log(hobby.toUpperCase())
+}
+if (person.role === Role.AUTHOR){
+ console.log('is author')
 }
