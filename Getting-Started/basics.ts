9afd6b55e85441f180f3ed3b1b491bbb1cd0a0e2
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-09 22:16:04 -0800

    Arrays

--- Getting-Started/basics.ts
+++ Getting-Started/basics.ts
@@ -1,9 +1,15 @@
 const person:{
     name: string;
-    age: number
+    age: number;
+    hobbies:string[];
 } = {
     name: "Nathnael",
-    age:  28
+    age:  28,
+    hobbies:["Sport","Cooking"]
 }
 
 console.log(person.name)
+
+for (const hobby of person.hobbies){
+    console.log(hobby.toUpperCase())
+}
