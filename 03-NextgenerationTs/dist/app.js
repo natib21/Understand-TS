7f919c7eb601392f6292814042e114210df7d032
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-14 01:02:49 -0800

    add Spread Operator and Rest Parameter

--- 03-NextgenerationTs/dist/app.js
+++ 03-NextgenerationTs/dist/app.js
@@ -13,4 +13,26 @@ const addD = (a, b = 1) => {
     return a + b;
 };
 console.log(addD(10));
+// 1.3) Rest Parameters
+const addR = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addNumbers = addR(4, 5, 20, 11);
+console.log(addNumbers);
 ///////////////////////////////////////
+// 2) The Spread Operator
+// 2.1) Array Spread Operator
+const hobbies = ["Sport", "Music"];
+const activeHobbies = ["Hiking"];
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+// 2.2) Object Spread Operator
+const person = {
+    name: "Nati",
+    age: 28
+};
+const copiedPerson = Object.assign({}, person);
+console.log(copiedPerson);
+//////////////////////////////////////
--- 03-NextgenerationTs/src/app.ts
+++ 03-NextgenerationTs/src/app.ts
@@ -1,5 +1,6 @@
 
 ////////////////////////////////////////
+
 // 1) Arrow Function
 
 const add =(a:number ,b:number)=>{
@@ -20,5 +21,40 @@ const addD = (a:number,b:number = 1) => {
 
 console.log(addD(10))
 
+// 1.3) Rest Parameters
 
+const addR = (...numbers : number[])=>{
+return numbers.reduce((curResult,curValue)=>{
+    return curResult + curValue
+},0)
+}
+
+const addNumbers = addR(4,5,20,11)
+console.log(addNumbers)
 ///////////////////////////////////////
+
+// 2) The Spread Operator
+
+ // 2.1) Array Spread Operator
+const hobbies = ["Sport","Music"]
+
+const activeHobbies = ["Hiking"]
+
+activeHobbies.push(...hobbies)
+
+console.log(activeHobbies)
+
+
+ // 2.2) Object Spread Operator
+
+const person ={
+    name:"Nati",
+    age:28
+}
+
+const copiedPerson = {...person}
+
+console.log(copiedPerson)
+
+
+//////////////////////////////////////
