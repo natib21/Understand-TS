fb4df066069b0e1c417dec9866a94265c05ba1c1
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-14 00:36:48 -0800

    Next Generation js ts

---
+++ 03-NextgenerationTs/dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const add = (a, b) => {
+    let result;
+    result = a + b;
+    return result;
+};
+const val = add(5, 6);
+console.log(val);
---
+++ 03-NextgenerationTs/src/app.ts
@@ -0,0 +1,10 @@
+
+
+const add =(a:number ,b:number)=>{
+    let result ;
+    result = a + b
+    return result
+}
+
+const val = add(5,6)
+console.log(val)
