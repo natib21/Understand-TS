43c917e9190b36f55ee0d3981030f5c820e166c3
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-14 22:21:52 -0800

    creating first class

--- 04-ClassesInterfaces/dist/app.js
+++ 04-ClassesInterfaces/dist/app.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log("Hello How Is You ?");
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+}
+const accounting = new Department("Accounting");
+console.log(accounting);
+/*
+    A class in JavaScript (and TypeScript) is a blueprint
+   for creating objects that share similar properties and methods.
+   It simplifies working with object-oriented programming by
+   allowing you to define reusable templates for objects.
+*/
+/*
+   A constructor is a special method of a class that is
+   automatically called when creating an object from the class.
+
+   It initializes the properties of the class.
+
+   Here, the constructor takes a parameter (n)
+   and assigns its value to the name property using this.name.
+*/ 
--- 04-ClassesInterfaces/src/app.ts
+++ 04-ClassesInterfaces/src/app.ts
@@ -1,1 +1,29 @@
-console.log("Hello How Is You ?")
+class Department {
+    name :string;
+    
+    constructor(n:string){
+        this.name = n
+    }
+}
+
+
+const accounting = new Department("Accounting")
+
+console.log(accounting)
+
+/* 
+    A class in JavaScript (and TypeScript) is a blueprint
+   for creating objects that share similar properties and methods.
+   It simplifies working with object-oriented programming by
+   allowing you to define reusable templates for objects. 
+*/
+
+/* 
+   A constructor is a special method of a class that is
+   automatically called when creating an object from the class.
+
+   It initializes the properties of the class.
+
+   Here, the constructor takes a parameter (n)
+   and assigns its value to the name property using this.name. 
+*/
