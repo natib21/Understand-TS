690cf3d72bafac1071b9ac1d25d9c447fa835390
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-15 23:48:30 -0800

    Interface

--- 04-ClassesInterfaces/dist/app.js
+++ 04-ClassesInterfaces/dist/app.js
@@ -1,87 +1,11 @@
 "use strict";
-class Department {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-        this.employee = [];
+// 1) Interface
+let user1;
+user1 = {
+    name: "Nathnael",
+    age: 28,
+    greet(phrase) {
+        console.log(phrase + ' ' + this.name);
     }
-    static createEmployee(name) {
-        return { name };
-    }
-    describe() {
-        console.log('Department ' + this.id + " " + this.name);
-    }
-    addEmployee(e) {
-        this.employee.push(e);
-    }
-    PrintEmployee() {
-        console.log(this.employee.length);
-        console.log(this.employee);
-    }
-}
-class ITDepartment extends Department {
-    constructor(id, admins) {
-        super(id, "It");
-        this.admins = admins;
-    }
-}
-class AccountingDepartment extends Department {
-    constructor(id, reports) {
-        super(id, "Accounting");
-        this.reports = reports;
-        this.lastReport = reports[0];
-    }
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-        throw new Error("No Report Found");
-    }
-    set mostRecentReport(value) {
-        if (!value) {
-            throw new Error('Please Pass in a valid value!');
-        }
-        this.addReports(value);
-    }
-    addEmployee(e) {
-        if (e === "Lisa") {
-            return;
-        }
-        this.employee.push(e);
-    }
-    addReports(reports) {
-        this.reports.push(reports);
-        this.lastReport = reports;
-    }
-    printReport() {
-        console.log(this.reports);
-    }
-}
-const accounting = new ITDepartment("0001", ['Max']);
-accounting.addEmployee("Musk");
-accounting.addEmployee("Natty");
-console.log(accounting);
-const employee = Department.createEmployee("Nati");
-console.log(employee);
-const AccountingDep = new AccountingDepartment("0002", ["Exam Result"]);
-console.log(AccountingDep.mostRecentReport);
-AccountingDep.mostRecentReport = 'Year and Report';
-AccountingDep.addEmployee("Lisa");
-AccountingDep.addEmployee("Alisa");
-AccountingDep.addReports("This is the Exam Report");
-console.log(AccountingDep);
-/*
-    A class in JavaScript (and TypeScript) is a blueprint
-   for creating objects that share similar properties and methods.
-   It simplifies working with object-oriented programming by
-   allowing you to define reusable templates for objects.
-*/
-/*
-   A constructor is a special method of a class that is
-   automatically called when creating an object from the class.
-
-   It initializes the properties of the class.
-
-   Here, the constructor takes a parameter (n)
-   and assigns its value to the name property using this.name.
-*/ 
+};
+user1.greet("Hi there - I am");
--- 04-ClassesInterfaces/src/app.ts
+++ 04-ClassesInterfaces/src/app.ts
@@ -1,156 +1,26 @@
-class Department {
-    protected employee:string[] = [];
+// 1) Interface
 
-    constructor(private readonly id : string ,public name : string){
-        
-    }
+// 1.1) What is an Interface ?
+//      It Describe the Sructure of An Object 
 
-    static createEmployee(name:string){
-        return {name}
-    }
-    describe(this:Department){
-        console.log('Department '+this.id +" "+ this.name)
-    }
+interface Person {
+    name:string;
+    age: number;
 
-    addEmployee(e: string){
-      this.employee.push(e)
-    }
-
-    PrintEmployee(){
-        console.log(this.employee.length)
-        console.log(this.employee)
-    }
+    greet(phrase: string): void
 
 }
 
-class ITDepartment extends Department {
+let user1: Person;
 
-  admins:string[];
+user1 = {
+    name:"Nathnael",
+    age: 28,
 
-   constructor(id:string,admins:string []){
-      super(id , "It");
-      this.admins = admins
-   }
-}
-
-class AccountingDepartment extends Department {
-
-    private lastReport: string
-    private reports: string[]
-
-    constructor(id:string, reports:string[]) {
-       super(id ,"Accounting");
-       this.reports = reports;
-       this.lastReport = reports[0]
+    greet(phrase: string){
+        console.log(phrase+ ' ' + this.name)
     }
-
-    get mostRecentReport(){
-
-        if(this.lastReport){
-            return this.lastReport
-        }
-        throw new Error("No Report Found")
-    }
-
-    set mostRecentReport(value: string){
-      if(!value){
-
-        throw new Error('Please Pass in a valid value!')
-
-      }
-       this.addReports(value)
-    }
-
-    addEmployee(e: string) {
-        if(e === "Lisa"){
-            return
-        }
-        this.employee.push(e)
-    }
-    addReports(reports:string){
-
-        this.reports.push(reports)
-        this.lastReport = reports
-
-    }
-    printReport(){
-        console.log(this.reports)
-    }
-
 }
 
 
-
-
-const accounting = new ITDepartment("0001",['Max'])
-
-accounting.addEmployee("Musk")
-accounting.addEmployee("Natty")
-
-console.log(accounting)
-
-const employee = Department.createEmployee("Nati")
-
-console.log(employee)
-
-const AccountingDep = new AccountingDepartment("0002",["Exam Result"])
-
-console.log(AccountingDep.mostRecentReport)
-
-AccountingDep.mostRecentReport = 'Year and Report'
-AccountingDep.addEmployee("Lisa")
-AccountingDep.addEmployee("Alisa")
-AccountingDep.addReports("This is the Exam Report")
-
-
-console.log(AccountingDep)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-    A class in JavaScript (and TypeScript) is a blueprint
-   for creating objects that share similar properties and methods.
-   It simplifies working with object-oriented programming by
-   allowing you to define reusable templates for objects. 
-*/
-
-/* 
-   A constructor is a special method of a class that is
-   automatically called when creating an object from the class.
-
-   It initializes the properties of the class.
-
-   Here, the constructor takes a parameter (n)
-   and assigns its value to the name property using this.name. 
-*/
+user1.greet("Hi there - I am")
