4f8127022e83c65e689c8ab3d2a81d4a00a5c612
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-21 00:04:59 -0800

    Type Guards in TypeScript

--- 05-AdvancedTypes/dist/app.js
+++ 05-AdvancedTypes/dist/app.js
@@ -5,3 +5,40 @@ const el = {
     startDate: new Date()
 };
 console.log(el);
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") { // Type Guard
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log('Name: ' + emp.name);
+    if ('privilages' in emp) { // Type Guard
+        console.log('Privilages: ' + emp.privilages);
+    }
+    if ('startDate' in emp) { // Type Guard
+        console.log('Start Date :' + emp.startDate);
+    }
+}
+printEmployeeInformation(el);
+class Car {
+    drive() {
+        console.log('Diriving ....');
+    }
+}
+class Truck {
+    drive() {
+        console.log('Diriving ....');
+    }
+    loadCargo(amount) {
+        console.log('Loading Cargo ...' + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) { // Type Guard 
+        vehicle.loadCargo(1000);
+    }
+}
--- 05-AdvancedTypes/src/app.ts
+++ 05-AdvancedTypes/src/app.ts
@@ -8,7 +8,7 @@ type Employee = {
     startDate:Date
 }
 
-type ElevatedEmployee = Admin & Employee
+type ElevatedEmployee = Admin & Employee // intersection types
 
 const el : ElevatedEmployee = {
     name: "Nati",
@@ -17,3 +17,63 @@ const el : ElevatedEmployee = {
 }
 
 console.log(el)
+
+type Combinable = string | number
+type Numeric = number | boolean
+
+type Universal = Combinable & Numeric  // intersection Types
+
+
+function add ( a: Combinable , b: Combinable){
+    if(typeof a === "string" || typeof b === "string"){ // Type Guard
+        return a.toString() + b.toString()
+    }
+    return a + b
+}
+
+type UnknownEmployee = Admin | Employee
+
+function printEmployeeInformation(emp: UnknownEmployee){
+    console.log('Name: '+emp.name)
+
+    if('privilages' in emp){  // Type Guard
+        console.log('Privilages: '+ emp.privilages) 
+    }
+
+    if('startDate' in emp){  // Type Guard
+        console.log('Start Date :'+ emp.startDate)
+    }
+
+}
+
+printEmployeeInformation(el)
+
+class Car {
+    drive() {
+        console.log('Diriving ....')
+    }
+}
+
+class Truck {
+        drive() {
+        console.log('Diriving ....')
+    }
+
+    loadCargo(amount:number){
+        console.log('Loading Cargo ...'+amount)
+    }
+}
+
+type Vehicle = Car | Truck
+
+const v1  = new Car()
+const v2 = new Truck()
+
+function useVehicle(vehicle:Vehicle){
+  vehicle.drive()
+
+  if ( vehicle instanceof Truck){ // Type Guard 
+    vehicle.loadCargo(1000)
+  }
+
+}
