428ba61bb775bc29cd4a9b6d01a12be7b0d07e4e
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-22 03:48:45 -0800

    Generics

---
+++ 06-Generics/dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+// Generics 
+// What are Generics ?
+//    in TypeScript (and other programming languages
+//  like Java and C#) are a way to create reusable
+//  and flexible components, such as functions, classes,
+//  and interfaces, that can work with any data type while 
+//  still enforcing type safety. Generics allow you 
+//  to define types that can be used with different data types
+//  without losing the benefits of type checking.
+const names = []; // Building Generics
