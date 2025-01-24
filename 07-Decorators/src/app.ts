43f094c6368d4aac4c3f8eae313eaf92e36dc617
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-23 23:27:20 -0800

    A First Decorator

--- 07-Decorators/src/app.ts
+++ 07-Decorators/src/app.ts
@@ -1,1 +1,17 @@
-console.log("Hello")
+function Logger(constructor: Function){
+    console.log('Logging...')
+    console.log(constructor)
+}
+
+
+@Logger
+class Person {
+     name = "Nathnael"
+
+    constructor(){
+        console.log("Hello How Is You ?")
+    }
+}
+
+const per = new Person()
+console.log(per)
