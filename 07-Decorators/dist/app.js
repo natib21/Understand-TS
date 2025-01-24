baa336b06ceeb5ea412e70f3f15bc9d930309f2e
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-24 00:12:11 -0800

    add Multiple Decorators

--- 07-Decorators/dist/app.js
+++ 07-Decorators/dist/app.js
@@ -5,9 +5,22 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(constructor) {
-    console.log('Logging...');
-    console.log(constructor);
+function Logger(log) {
+    return (constructor) => {
+        console.log(log);
+        console.log(constructor);
+    };
+}
+function WithTemplate(template, hookId) {
+    return function (constructor) {
+        const el = document.getElementById(hookId);
+        console.log(el);
+        const p = new constructor();
+        if (el) {
+            el.innerHTML = template;
+            el.querySelector('h1').textContent = p.name;
+        }
+    };
 }
 let Person = class Person {
     constructor() {
@@ -16,7 +29,8 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger
+    Logger('hi'),
+    WithTemplate("<h2>Hi Nathnael</h2>", "app")
 ], Person);
 const per = new Person();
 console.log(per);
--- 07-Decorators/src/app.ts
+++ 07-Decorators/src/app.ts
@@ -1,17 +1,29 @@
-function Logger(constructor: Function){
-    console.log('Logging...')
-    console.log(constructor)
+function Logger(log: string){
+    return (constructor: Function)=>{
+        console.log(log)
+        console.log(constructor)
+
+    }
 }
-
-
-@Logger
+ 
+function WithTemplate(template:string,hookId : string){
+    return function(constructor:any) {
+      const el  = document.getElementById(hookId)
+      console.log(el)
+      const p = new constructor()
+      if(el){
+          el.innerHTML = template
+          el.querySelector('h1')!.textContent = p.name
+      }
+    }
+}
+@Logger('hi')
+@WithTemplate("<h2>Hi Nathnael</h2>","app")
 class Person {
-     name = "Nathnael"
-
+    name = "Nathnael"
     constructor(){
         console.log("Hello How Is You ?")
     }
 }
-
 const per = new Person()
 console.log(per)
