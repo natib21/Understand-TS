513df652514a2cec292055eeee5c265e8a7ca97b
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-27 00:54:19 -0800

    Interacting with Dom Element

--- 08-Drag_Drop_Project/src/app.ts
+++ 08-Drag_Drop_Project/src/app.ts
@@ -1,14 +1,35 @@
 class ProjectInput {
     templateElement : HTMLTemplateElement
     hostElement : HTMLDivElement
-    element:HTMLFormElement
+    element : HTMLFormElement
+    titleInputElement : HTMLInputElement
+    descriptionInputElement : HTMLInputElement
+    peopleInputElement : HTMLInputElement
+
     constructor(){
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
         this.hostElement = document.getElementById('app')! as HTMLDivElement
 
         const importedNode = document.importNode(this.templateElement.content,true)
         this.element = importedNode.firstElementChild as HTMLFormElement
+        this.element.id = 'user-input'
+       
+        this.titleInputElement = this.element.querySelector('#title')  as HTMLInputElement
+        this.descriptionInputElement = this.element.querySelector('#description')  as HTMLInputElement
+        this.peopleInputElement = this.element.querySelector('#people')  as HTMLInputElement
+        this.configure()
         this.attach()
+    }
+
+    private submitHandler(event:Event){
+     event.preventDefault()
+     console.log(this.titleInputElement.value)
+    }
+
+    private configure(){
+        this.element.addEventListener(
+            'submit',this.submitHandler.bind(this)
+        )
     }
 
     private attach(){
@@ -18,3 +39,5 @@ class ProjectInput {
 
 
 const project  = new ProjectInput()
+
+/// Lecture 123
