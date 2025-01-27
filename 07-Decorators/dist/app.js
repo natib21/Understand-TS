7e76d29dbd4ab06731a6a457f17b1b06a34c969e
nathnael zelalem <nathnaelzelalem@gmail.com>
2025-01-26 22:19:16 -0800

    Creating Auto Bind

--- 07-Decorators/dist/app.js
+++ 07-Decorators/dist/app.js
@@ -94,3 +94,30 @@ __decorate([
     Log3,
     __param(0, Log4)
 ], Product.prototype, "getPriceWithTax", null);
+function Autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjDescriptor;
+}
+class Printer {
+    constructor() {
+        this.message = 'This Works !';
+    }
+    showMessage() {
+        console.log(this.message);
+    }
+}
+__decorate([
+    Autobind
+], Printer.prototype, "showMessage", null);
+const p = new Printer();
+const btn = document.querySelector('button');
+console.log(btn);
+btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', p.showMessage);
--- 07-Decorators/src/app.ts
+++ 07-Decorators/src/app.ts
@@ -86,3 +86,26 @@ class Product {
        return this._price * (1 + tax)
     }
 }
+function Autobind(_:any,_2: string,descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable:true,
+        enumerable:false,
+        get(){
+          const boundFn = originalMethod.bind(this)
+          return boundFn
+        },
+    }
+    return adjDescriptor
+}
+class Printer {
+    message = 'This Works !'
+    @Autobind
+    showMessage(){
+        console.log(this.message)
+    }
+}
+const p = new Printer()
+const btn = document.querySelector('button')!
+console.log(btn)
+btn?.addEventListener('click',p.showMessage)
